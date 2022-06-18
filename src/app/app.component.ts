a30f521e0ecc80d3f30748ef7849c00bb232a710
Martin-Macmini <martinn.henriquez@gmail.com>
2022-06-18 10:45:20 -0500

    App dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Dados';
+  dadpoIzquierdo = '../assets/img/dice1.png';
+  daddoDerecho = '../assets/img/dice4.png';
+  numero1 = 1;
+  numero2 = 4;
+
+
+  tirarDados(): void {
+    this.numero1 = Math.round(Math.random() * 5) + 1;
+    this.numero2 = Math.round(Math.random() * 5) + 1;
+    this.dadpoIzquierdo = '../assets/img/dice' + this.numero1 + '.png';
+    this.daddoDerecho = '../assets/img/dice' + this.numero2 + '.png';
+  }
+
 }
